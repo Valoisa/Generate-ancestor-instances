14258f9b88d493cdd0b99a2e7eda786d4231fa86
Valoisa <mailbox_of_olga@mail.ru>
2016-05-04 12:44:19 +0300

    Add a cid_binds forming function (for new instance Monad)

--- AppendInstance.hs
+++ AppendInstance.hs
@@ -57,10 +57,16 @@ mkInstance insthead funs = ClsInstDecl {
                                       , cid_datafam_insts = []
                                       , cid_overlap_mode  = Nothing
                                             }
-mkBind :: String -> String -> HsBindLR RdrName RdrName
-mkBind idL idR = VarBind { var_id     = mkRdrName idL
+mkBind :: String -> String -> LHsBindLR RdrName RdrName
+mkBind idL idR = noLoc $ 
+                VarBind { var_id     = mkRdrName idL
                          , var_rhs    = unMaybe (parseToLHsExpr idR)
                          , var_inline = False }
+
+mkCIDBindsForInstMonad :: LHsBindLR RdrName RdrName -> LHsBinds RdrName
+mkCIDBindsForInstMonad userbind = listToBag [ (mkBind "return" "pure")
+                                            , (mkBind "(>>)" "(*>)")
+                                            , userbind]
 
 -- "Pulls out" the declarations from the module 
 -- (that are inside of Located)
